ea6e358966c6a5a65e752fe59e054fd954939a7d
Petlus <csspanda@gmail.com>
2026-03-14 01:49:43 +0100

    chore: Update version to 1.3.4, modify APK download link, and refresh README with new content and version details.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.seniorenquiz"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.3.3"
+        versionCode = 10
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
